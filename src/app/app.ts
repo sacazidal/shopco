9fc587afe63699222c09221a0d94986729f25bdd
sacazidal <tw1nkle001@yandex.ru>
2026-01-07 01:33:41 +0700

    добавил в импорт over-header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OverHeaderComponent } from './components/over-header/over-header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OverHeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.less',
 })
